717c81bf23df3178891dad9ed14fcc8b2dec6a29
jeancaffou <jean@briskula.si>
2023-02-26 13:54:22 +0100

    quasar init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          UPN QR
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
