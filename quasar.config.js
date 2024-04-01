638331a1754f4cf4dbe98a450a9512434d629e9d
jeancaffou <jean@briskula.si>
2024-04-01 11:23:02 +0200

    privacy issues - closes #4

--- quasar.config.js
+++ quasar.config.js
@@ -58,7 +58,7 @@ module.exports = configure(function (/* ctx */) {
         node: 'node16'
       },
 
-      vueRouterMode: 'history' // available values: 'hash', 'history'
+      vueRouterMode: 'hash' // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
